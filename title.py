f03d42805cfe1da2727cb9adca545d00f2c410e8
Brndm <brian.adam25@orange.fr>
2020-02-29 08:39:15 -0500

    Added print to excel method

--- title.py
+++ title.py
@@ -4,17 +4,24 @@ import plotly.graph_objects as go
 
 
 class Title:
-    tick = ''
-    histo_data = pd.DataFrame()
+    tick = None
+    ref_data = None
+    histo_data = None
 
     # Constructors
+    def __init__(self, args):
+        if len(args) == 2:
+            self.init_ref(args[0], args[1])
+        else:
+            self.init_histo(args[0], args[1], args[2], args[3])
+
     # Reference data constructor
-    def __init__(self, ticker, fields):
+    def init_ref(self, ticker, fields):
         self.tick = ticker
-        self.histo_data = blp.bdp(tickers=ticker, flds=fields)
+        self.ref_data = blp.bdp(tickers=ticker, flds=fields)
 
     # Historical data constructor
-    def __init__(self, ticker, fields, startdate, enddate):
+    def init_histo(self, ticker, fields, startdate, enddate):
         self.tick = ticker
         self.histo_data = blp.bdh(tickers=ticker, flds=fields, start_date=startdate, end_date=enddate)
 
@@ -27,7 +34,12 @@ class Title:
         fig.update_layout(title=self.tick, xaxis_rangeslider_visible=False)  # Remove to add range slider
         fig.show()
 
-    # BDS
-    # Intraday bars
-    # Corporate earnings
-    # write to excel
+    # Accessor
+    def get_data(self):
+        try:
+            return self.histo_data
+        except NameError:
+            try:
+                return self.ref_data
+            except NameError:
+                print("An error occurred, Trying to access non initialized member in Title class")
